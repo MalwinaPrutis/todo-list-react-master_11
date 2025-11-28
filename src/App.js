5f4ef2a0e6ce5377819af7fbcf7342091f05719f
TwojaNazwaGitHub <twoj-email@gmail.com>
2025-11-28 23:53:21 +0100

    feat: Wprowadzenie routingu, nawigacji i pliku routes.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import Form from "./common/Form";
-import TasksList from "./features/Tasks/TasksList";
-import Buttons from "./Buttons";
-import Section from "./common/Section";
-import Header from "./common/Header";
-import { Container } from './common/Container';
+// src/App.js
+
+import React from 'react';
+// Ważne: importujemy komponenty do routingu
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom"; 
+
+// Link jest importowany z głównego index.js (gdzie pewnie masz styled.a)
+import { Link } from "./index.js"; 
+
+// Importujemy stronę z zadaniami (dostosowujemy do nowej struktury)
+import TasksPage from './features/tasks/TasksPage'; 
+// Importujemy stronę autora (już stworzoną przez Ciebie)
+import AuthorPage from './features/author/AuthorPage'; 
+
+// Importujemy stałe ze ścieżkami (ten plik zaraz stworzymy)
+import { toTasks, toAuthor } from './routes'; 
+
+// Importujemy ogólny kontener (z nowej lokalizacji)
+import Container from "./common/Container/index.js"; 
 
 function App() {
   return (
-    <Container>
-      <Header title="Lista zadań" />
+    <HashRouter>
+      <Container>
+        {/* Nawigacja z linkami */}
+        <nav>
+          <ul>
+            <li>
+              <Link to={toTasks()}>Zadania</Link>
+            </li>
+            <li>
+              <Link to={toAuthor()}>O autorze</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <Section
-        title="Dodaj nowe zadanie"
-        body={<Form />}
-      />
-
-      <Section
-        title="Lista zadań"
-        body={<TasksList />}
-        extraHeaderContent={<Buttons />}
-      />
-    </Container>
+        {/* Sekcja routingu, gdzie będą przełączać się strony */}
+        <Switch>
+          <Route path={toTasks()}>
+            <TasksPage />
+          </Route>
+          <Route path={toAuthor()}>
+            <AuthorPage />
+          </Route>
+          {/* Ustawiamy domyślną stronę (redirect na Zadania) */}
+          <Route path="/">
+            <Redirect to={toTasks()} />
+          </Route>
+        </Switch>
+      </Container>
+    </HashRouter>
   );
 }
+
 export default App;
